7233043ad0cc409b7a60ce9ebf960e2d82d41846
Katya <savvoyy@yahoo.com>
2022-03-30 18:08:08 +0300

    add a game logic in index

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
-// приветствуем игрока
-const greeting = () => {  
+import readlineSync from 'readline-sync';
+
+// функция генерации случайноего числа
+export const randomNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+export const game = (rule, questionAnswer) => {
+  // приветствие
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name?');
   console.log(`Hello, ${userName}!`);
-};
+  console.log(rule); 
 
-// функция генерации случайноего числа
-const randomNum = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+  for (let i = 0; i < 3; i += 1) {
+  // задаем вопрос игруку !!!!
+  const [question, answer] = questionAnswer ();
+  console.log(`Question: ${question}`);
 
-// получаем ответ
-const userAnswer = () => readlineSync.question('Your answer:');
+  // получаем ответ
+  const userAnswer = readlineSync.question('Your answer:');
 
-// сравниваем ответ игрока с верным
-const compare = (userAnswer, answer) => {
-if (userAnswer === answer) {
-  console.log('Correct!');
-} else {
-  console.log(
-    `'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`
-  );
-  console.log(`Let's try again, ${userName}!`); // отвечаем игроку
-  return;
+  // сравниваем ответ игрока с верным
+  if (userAnswer === answer) {
+    console.log('Correct!');
+    } else {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
+    console.log(`Let's try again, ${userName}!`); // отвечаем игроку
+    return;    
+ 
+ console.log(`Congratulations, ${userName}`);
 };
 };
-export default { greeting, randomNum, userAnswer, compare };
