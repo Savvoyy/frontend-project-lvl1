e35bc9168c830ef81a937d6f70df32526bdf5111
Katya <savvoyy@yahoo.com>
2022-03-31 11:39:14 +0300

    fix errors in second game

--- src/games/secondGame.js
+++ src/games/secondGame.js
@@ -12,11 +12,11 @@ const calc = (num1, num2, operator) => {
 };
 
 // задаем вопрос _ получаем ответ
-const operators = ['+', '-', '*'];
-const newOperator = operators[randomNum(0, 2)];
-const newNum1 = randomNum(1, 10);
-const newNum2 = randomNum(1, 10);
 const questionAnswer = () => {
+  const operators = ['+', '-', '*'];
+  const newOperator = operators[randomNum(0, 3)];
+  const newNum1 = randomNum(1, 11);
+  const newNum2 = randomNum(1, 11);
   const question = `${newNum1} ${newOperator} ${newNum2}`;
   const answer = String(calc(newNum1, newNum2, newOperator));
   return [question, answer];
